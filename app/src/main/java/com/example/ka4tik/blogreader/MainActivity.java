c39502d601064cfc6a5594fd48c6e5f345f0bf19
Kartik Singal <ka4tik@gmail.com>
2015-06-10 04:01:16 +0530

    displaying titles of blog

--- app/src/main/java/com/example/ka4tik/blogreader/MainActivity.java
+++ app/src/main/java/com/example/ka4tik/blogreader/MainActivity.java
@@ -1,41 +1,104 @@
 package com.example.ka4tik.blogreader;
 
+import android.app.AlertDialog;
 import android.app.ListActivity;
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.AsyncTask;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.text.Html;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.ArrayAdapter;
+import android.widget.Toast;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.StatusLine;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 
 public class MainActivity extends ListActivity {
 
 
-    protected String[] mAndroidNames = {
-            "Android Beta",
-            "Android 1.0",
-            "Android 1.1",
-            "CupCake",
-            "Donot",
-            "Eclair",
-            "Froyo",
-            "Gingerbread",
-            "Honeycomb",
-            "Ice Cream Sandwich",
-            "Jelly Bean",
-            "KitKat",
-            "Lollipop",
-            "Android M"
-    };
+    protected String[] mBlogPostTitles;
+    public static final int NUMBER_OF_POSTS = 20;
+    public static final String TAG = MainActivity.class.getSimpleName();
+    protected JSONObject mBlogData;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,mAndroidNames);
-        setListAdapter(adapter);
+        if(isNetworkAvailable()) {
+            GetBlogPostTask getBlogPostTask = new GetBlogPostTask();
+            getBlogPostTask.execute();
+        }
+        else {
+            Toast.makeText(this,"Network is unavailable", Toast.LENGTH_LONG).show();
+        }
+
     }
 
+    private boolean isNetworkAvailable() {
+        boolean isAvailable = false;
+        ConnectivityManager manager= (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = manager.getActiveNetworkInfo();
+        if(networkInfo !=null && networkInfo.isConnected())
+            isAvailable = true;
+        return isAvailable;
+
+    }
+    private void updateList() {
+        if(mBlogData == null){
+
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            builder.setTitle("Opps! Sorry");
+            builder.setMessage("There was a error getting data from network");
+            builder.setPositiveButton(android.R.string.ok,null);
+            AlertDialog dialog = builder.create();
+            dialog.show();
+        }
+        else
+        {
+            try{
+                  JSONArray jsonPosts =mBlogData.getJSONArray("posts");
+                   mBlogPostTitles = new String[jsonPosts.length()];
+                    for(int i=0;i<jsonPosts.length();i++){
+                        JSONObject post = jsonPosts.getJSONObject(i);
+                        String title = post.getString("title");
+                        title  = Html.fromHtml(title).toString();
+                        mBlogPostTitles[i] = title;
+                    }
+
+                ArrayAdapter<String > adapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,mBlogPostTitles);
+                setListAdapter(adapter);
+
+            }
+            catch (JSONException e){
+                Log.e(TAG,"Exception caught!",e);
+            }
+
+        }
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -57,5 +120,55 @@ public class MainActivity extends ListActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    private class GetBlogPostTask extends AsyncTask<Object,Void,JSONObject> {
+
+
+        @Override
+        protected JSONObject doInBackground(Object... params) {
+            int responseCode = -1;
+            JSONObject jsonResponse = null;
+            StringBuilder builder = new StringBuilder();
+            HttpClient client = new DefaultHttpClient();
+            HttpGet httpget = new HttpGet("http://blog.teamtreehouse.com/api/get_recent_summary/?count=" + NUMBER_OF_POSTS);
+
+            try {
+                HttpResponse response = client.execute(httpget);
+                StatusLine statusLine = response.getStatusLine();
+                responseCode = statusLine.getStatusCode();
+
+                if (responseCode == HttpURLConnection.HTTP_OK) {
+                    HttpEntity entity = response.getEntity();
+                    InputStream content = entity.getContent();
+                    BufferedReader reader = new BufferedReader(new InputStreamReader(content));
+                    String line;
+                    while((line = reader.readLine()) != null){
+                        builder.append(line);
+                    }
+
+                    jsonResponse = new JSONObject(builder.toString());
+                }
+                else {
+                    Log.i(TAG, String.format("Unsuccessful HTTP response code: %d", responseCode));
+                }
+            }
+            catch (JSONException e) {
+                Log.e(TAG,"Exception caught!",e);
+            }
+            catch (Exception e) {
+                Log.e(TAG, "Exception caught!", e);
+            }
+
+            return jsonResponse;
+        }
+
+        @Override
+        protected void onPostExecute(JSONObject result){
+            mBlogData = result;
+            updateList();
+
+        }
+
     }
 }
